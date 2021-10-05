a42bd86d3cdf7e09c51d0dd17cc04c01c230504b
Uzoma Udenkwor <uzomaudenkwor@gmail.com>
2021-10-05 01:53:11 +0100

    added handlebars

---
+++ views/helpers/jsn.js
@@ -0,0 +1,24 @@
+// // var exphbs = require('express-handlebars');
+// // var handlebars = exphbs.handlebars;
+
+// module.exports = function(handlebars) {
+//     handlebars.registerHelper("jsn", function(context) {
+//         return JSON.stringify(context)
+//     })
+// }
+
+// // handlebars.registerHelper('where', function(context, options) {
+// //     const medArray = []
+
+// //     context.array.forEach(element => {
+// //         Object.keys(element).forEach( (keys, values) => {
+// //             medArray.push( {keys: values} )
+// //         }
+// //     });
+// // })
+
+// // module.exports = function(handlebars) {
+// //     return function() {
+//             return new handlebars.JSON.stringify(context)
+// //   }
+// // }
